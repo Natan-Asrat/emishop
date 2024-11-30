4dab2d20001cd093a562750b57bd42515448080f
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-11-30 05:48:19 +0300

    allow all cors

--- back/back/settings.py
+++ back/back/settings.py
@@ -94,13 +94,31 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
 }
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:5174",
+# CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS').split(',')
+
+# CORS_TRUSTED_ORIGINS = os.environ.get('CORS_TRUSTED_ORIGINS').split(',')
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
-CORS_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:5174",
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 AUTH_USER_MODEL = "account.User"
 
