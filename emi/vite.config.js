63b2f368019442d021727a1c93ccc9e260d592f1
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-12-08 23:25:55 +0300

    debug url

--- emi/vite.config.js
+++ emi/vite.config.js
@@ -112,5 +112,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? 'https://emishoplive.onrender.com/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/' : '/'
 })
