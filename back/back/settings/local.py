bbb5a5bbe25caba3c1db5b72957769c1f7356feb
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-12-08 19:52:47 +0300

    site url

--- back/back/settings/local.py
+++ back/back/settings/local.py
@@ -6,7 +6,7 @@ SECRET_KEY = "django-insecure-xp51pjd!)y3c51t)do+25^cu#fq@2qa#xh$bfidj0zxaj=ob$y
 REPORTED_BLOCKLIST_DAYS = 30
 PAYPAL_MODE = 'sandbox'
 
-SITE_URL = 'http://localhost:8000'
+SITE_URL = os.environ.get('SITE_URL', 'http://localhost:8000')
 ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS += [
     'debug_toolbar',
